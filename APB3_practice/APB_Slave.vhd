ee759ee2478e3bb97d20b168022ecf240a386422
PKazm <56516730+PKazm@users.noreply.github.com>
2019-10-14 16:56:19 -0400

    Add files via upload

---
+++ APB3_practice/APB_Slave.vhd
@@ -0,0 +1,187 @@
+--------------------------------------------------------------------------------
+-- Company: <Name>
+--
+-- File: APB_Slave.vhd
+-- File history:
+--      <Revision number>: <Date>: <Comments>
+--      <Revision number>: <Date>: <Comments>
+--      <Revision number>: <Date>: <Comments>
+--
+-- Description: 
+--
+-- <Description here>
+--
+-- Targeted device: <Family::SmartFusion2> <Die::M2S010> <Package::144 TQ>
+-- Author: <Name>
+--
+--------------------------------------------------------------------------------
+
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity APB_Slave is
+port (
+    --<port_name> : <direction> <type>;
+	
+	PCLK : in std_logic;
+
+	PRESETn : in std_logic; -- active low
+	PADDR : in std_logic_vector(31 downto 0);
+	PSELx : in std_logic;
+	PENABLE : in std_logic;
+	PWRITE : in std_logic;
+	PWDATA : in std_logic_vector(31 downto 0);
+	PREADY : out std_logic;
+	PRDATA : out std_logic_vector(31 downto 0);
+	PSLVERR : out std_logic;
+
+	INT_IN : in std_logic_vector (1 downto 0);		-- I2C and Timer
+	INT_REQ : out std_logic;
+
+	led_out : out std_logic_vector(7 downto 0);
+	j7_out : out std_logic_vector(4 downto 0)
+    --<other_ports>;
+);
+end APB_Slave;
+architecture architecture_APB_Slave of APB_Slave is
+   -- signal, component etc. declarations
+	signal apb_fsm : unsigned (1 downto 0) := (others => '0');
+	signal prdata_sig : std_logic_vector (7 downto 0) := (others => '0');
+	signal led_reg : std_logic_vector (7 downto 0) := (others => '0');
+	signal ABC_MEM : std_logic_vector (7 downto 0) := (others => '0');
+	signal light_CH0_reg : unsigned (15 downto 0) := (others => '0');
+	signal light_CH1_reg : unsigned (15 downto 0) := (others => '0');
+	signal pready_sig : std_logic := '1';
+	signal int_sig : std_logic := '0';
+
+	constant SELF_INT : std_logic_vector (1 downto 0) := "00";
+	constant I2C_INT : std_logic_vector (1 downto 0) := "01";
+	constant TIMER_INT : std_logic_vector (1 downto 0) := "10";
+
+	constant INT_ADDR : std_logic_vector (7 downto 0) := X"00";
+	constant LIGHT_CH0_ADDR : std_logic_vector (7 downto 0) := X"01";
+	constant LIGHT_CH1_ADDR : std_logic_vector (7 downto 0) := X"02";
+	constant LED_ADDR : std_logic_vector (7 downto 0) := "11110000";
+	constant ABC_MEM_ADDR : std_logic_vector (7 downto 0) := X"FF";
+
+begin
+	process(PCLK)
+		variable paddr_var : std_logic_vector(7 downto 0);
+		variable pwdata_var : std_logic_vector(7 downto 0);
+	begin
+		-- slave states follow the master state
+		if(rising_edge(PCLK)) then
+			case to_integer(unsigned(apb_fsm)) is
+				when 0 =>		-- Idle State
+					if(PSELx = '1') then		-- master is in SETUP and will move to ACCESS next clock
+						apb_fsm <= to_unsigned(1, 2);
+						if(PWRITE = '1') then
+							-- I am ready to read immediately
+							pready_sig <= '1';
+						else
+							-- I am not ready to write immediately. (probably)
+							pready_sig <= '0';
+						end if;
+					else
+						prdata_sig <= X"00";
+					end if;
+				when 1 =>		-- Master has done SETUP and is currently in ACCESS
+					-- I only want the important bits from APB address
+					paddr_var(7) := PADDR(7);
+					paddr_var(6) := PADDR(6);
+					paddr_var(5) := PADDR(5);
+					paddr_var(4) := PADDR(4);
+					paddr_var(3) := PADDR(3);
+					paddr_var(2) := PADDR(2);
+					paddr_var(1) := PADDR(1);
+					paddr_var(0) := PADDR(0);
+
+					pwdata_var := PWDATA(7 downto 0);
+					if(PENABLE = '1') then
+						case paddr_var is
+							when INT_ADDR =>
+								if(PWRITE = '1') then
+									null;
+								else
+									if(INT_IN(0) = '1') then
+										prdata_sig <= (7 downto 2 => '0') & I2C_INT;
+									elsif(INT_IN(1) = '1') then
+										prdata_sig <= (7 downto 2 => '0') & TIMER_INT;
+									elsif(INT_IN = "00") then
+										prdata_sig <= (7 downto 2 => '0') & SELF_INT;
+									end if;
+								end if;
+							when LIGHT_CH0_ADDR =>
+								light_CH0_reg <= unsigned(pwdata_var) & light_CH0_reg(15 downto 8);
+							when LIGHT_CH1_ADDR =>
+								light_CH1_reg <= unsigned(pwdata_var) & light_CH1_reg(15 downto 8);
+							when LED_ADDR =>
+								if(PWRITE = '1') then
+									led_reg(7) <= PWDATA(7);
+									led_reg(6) <= PWDATA(6);
+									led_reg(5) <= PWDATA(5);
+									led_reg(4) <= PWDATA(4);
+									led_reg(3) <= PWDATA(3);
+									led_reg(2) <= PWDATA(2);
+									led_reg(1) <= PWDATA(1);
+									led_reg(0) <= PWDATA(0);
+								else
+									prdata_sig <= led_reg;
+								end if;
+							when ABC_MEM_ADDR =>
+								if(PWRITE = '1') then
+									ABC_MEM(7) <= PWDATA(7);
+									ABC_MEM(6) <= PWDATA(6);
+									ABC_MEM(5) <= PWDATA(5);
+									ABC_MEM(4) <= PWDATA(4);
+									ABC_MEM(3) <= PWDATA(3);
+									ABC_MEM(2) <= PWDATA(2);
+									ABC_MEM(1) <= PWDATA(1);
+									ABC_MEM(0) <= PWDATA(0);
+								else
+									prdata_sig <= ABC_MEM;
+								end if;
+							when others =>
+								null;
+						end case;
+					end if;
+
+					if(PWRITE = '1')then
+						apb_fsm <= to_unsigned(0, 2);
+						pready_sig <= '1';
+					else
+						apb_fsm <= to_unsigned(2, 2);
+						pready_sig <= '1';
+					end if;
+				when 2 =>
+					-- no idea what might go here, maybe Write to master stuff?
+					apb_fsm <= to_unsigned(0, 2);
+				when others =>
+					apb_fsm <= to_unsigned(0, 2);
+			end case;
+
+		end if;
+
+	end process;
+
+	led_out <= not led_reg;
+	j7_out <= std_logic_vector(light_CH1_reg(4 downto 0));
+
+	PRDATA <= (31 downto 8 => '0') & prdata_sig;
+	--PRDATA(7) <= prdata_sig(7);
+	--PRDATA(6) <= prdata_sig(6);
+	--PRDATA(5) <= prdata_sig(5);
+	--PRDATA(4) <= prdata_sig(4);
+	--PRDATA(3) <= prdata_sig(3);
+	--PRDATA(2) <= prdata_sig(2);
+	--PRDATA(1) <= prdata_sig(1);
+	--PRDATA(0) <= prdata_sig(0);
+	PREADY <= pready_sig;
+	PSLVERR <= '0';
+
+	INT_REQ <= INT_IN(0) or INT_IN (1) or int_sig;
+
+   -- architecture body
+end architecture_APB_Slave;
---
+++ APB3_practice/CoreABC_C0_0.asm
@@ -0,0 +1,450 @@
+JUMP $MAIN
+$INTERRUPT
+    //poll APB_Slave for int source
+    APBREAD 0 0x00
+    //00 = APB_Slave, 01 = I2C, 10 = timer
+
+    CMP DAT8 0b01
+    JUMP IF ZERO $INTI2C
+    CMP DAT8 0b10
+    JUMP IF ZERO $INTTIMER
+    JUMP $INTAPBSLAVE
+
+
+    $INTTIMER
+    //Any value written to Timer ADDR 0x10 clears the interrupt
+    APBWRT DAT8 2 0x10 1
+
+    //Read current LED register, increment by 1, write back.
+    APBREAD 0 0xF0
+    INC
+    APBWRT ACC 0 0xF0
+    
+    //Read Light Sensor via I2C
+    APBREAD 1 0x00
+    OR DAT8 0b01100000
+    APBWRT ACC 1 0x00
+    
+    RETISR
+    
+
+    //Handle I2C interrupt, probably send next byte
+    $INTI2C
+    APBREAD 1 0x04
+    CMP DAT8 0xE0
+    JUMP IF ZERO $ClearIntI2C
+    
+
+    IOREAD
+    CMP DAT8 27
+    JUMP IF ZERO $I2C_27
+    CMP DAT8 26
+    JUMP IF ZERO $I2C_26
+    CMP DAT8 25
+    JUMP IF ZERO $I2C_25
+    CMP DAT8 24
+    JUMP IF ZERO $I2C_24
+    CMP DAT8 23
+    JUMP IF ZERO $I2C_23
+    CMP DAT8 22
+    JUMP IF ZERO $I2C_22
+    CMP DAT8 21
+    JUMP IF ZERO $I2C_21
+    CMP DAT8 20
+    JUMP IF ZERO $I2C_20
+    CMP DAT8 19
+    JUMP IF ZERO $I2C_19
+    CMP DAT8 18
+    JUMP IF ZERO $I2C_18
+    CMP DAT8 17
+    JUMP IF ZERO $I2C_17
+    CMP DAT8 16
+    JUMP IF ZERO $I2C_16
+    CMP DAT8 15
+    JUMP IF ZERO $I2C_15
+    CMP DAT8 14
+    JUMP IF ZERO $I2C_14
+    CMP DAT8 13
+    JUMP IF ZERO $I2C_13
+    CMP DAT8 12
+    JUMP IF ZERO $I2C_12
+    CMP DAT8 11
+    JUMP IF ZERO $I2C_11
+    CMP DAT8 10
+    JUMP IF ZERO $I2C_10
+    CMP DAT8 9
+    JUMP IF ZERO $I2C_9
+    CMP DAT8 8
+    JUMP IF ZERO $I2C_8
+    CMP DAT8 7
+    JUMP IF ZERO $I2C_7
+    CMP DAT8 6
+    JUMP IF ZERO $I2C_6
+    CMP DAT8 5
+    JUMP IF ZERO $I2C_5
+    CMP DAT8 4
+    JUMP IF ZERO $I2C_4
+    CMP DAT8 3
+    JUMP IF ZERO $I2C_3
+    CMP DAT8 2
+    JUMP IF ZERO $I2C_2
+    CMP DAT8 1
+    JUMP IF ZERO $I2C_1
+    CMP DAT8 0
+    JUMP IF ZERO $I2C_0
+    CMP DAT8 0xFF
+    JUMP IF ZERO $AbandonShip
+
+    //BEGIN Light Sens INIT states
+
+        //Step 0, write address light sens for initial setup
+        $I2C_0
+        APBREAD 1 0x04
+        CMP DAT8 0x08
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b0
+        IOWRT DAT8 1
+        JUMP $ContinueI2C
+
+        //step 1, write control register address
+        $I2C_1
+        APBREAD 1 0x04
+        CMP DAT8 0x18
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0x80
+        IOWRT DAT8 2
+        JUMP $ContinueI2C
+
+        //step 2, write control register data
+        $I2C_2
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0b00000001
+        IOWRT DAT8 3
+        JUMP $ContinueI2C
+
+        //step 3, register write finished, stop I2C
+        $I2C_3
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 4
+        JUMP $StopI2C
+
+    //END Light Sens INIT states
+
+    //BEGIN Light Sens Read states
+
+    //BEGIN Light Sens Read CH1_0
+        //Step 4, write address light sens for CH1_0 read process
+        $I2C_4
+        APBREAD 1 0x04
+        CMP DAT8 0x08
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b0
+        IOWRT DAT8 5
+        JUMP $ContinueI2C
+
+        //Step 5, write CH1_0 register address
+        $I2C_5
+        APBREAD 1 0x04
+        CMP DAT8 0x18
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0x88
+        IOWRT DAT8 6
+        JUMP $ContinueI2C
+
+        //Step 6, repeat start to begin change from write to read
+        $I2C_6
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 7
+        JUMP $RepeatStartI2C
+
+        //Step 7, write address light sens, set to Read
+        //I2C will swap to Master/Receiver mode
+        $I2C_7
+        APBREAD 1 0x04
+        CMP DAT8 0x10
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b1
+        IOWRT DAT8 8
+        JUMP $ContinueI2C
+
+        //Step 8, prep to read CH1_0 register data
+        $I2C_8
+        APBREAD 1 0x04
+        CMP DAT8 0x40
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 9
+        JUMP $ContinuteI2CRead
+
+        //Step 9, actually finally read CH1_0 register data
+        $I2C_9
+        APBREAD 1 0x04
+        CMP DAT8 0x58
+        JUMP IFNOT ZERO $AbandonShip
+        APBREAD 1 0x08
+        APBWRT ACC 0 0x02
+        IOWRT DAT8 10
+        JUMP $StopStartI2C
+
+    //END Light Sens Read CH1_0
+
+    //BEGIN Light Sens Read CH1_1
+        //Step 10, write address light sens for CH1_1 read process
+        $I2C_10
+        APBREAD 1 0x04
+        CMP DAT8 0x08
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b0
+        IOWRT DAT8 11
+        JUMP $ContinueI2C
+
+        //Step 11, write CH1_1 register address
+        $I2C_11
+        APBREAD 1 0x04
+        CMP DAT8 0x18
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0x89
+        IOWRT DAT8 12
+        JUMP $ContinueI2C
+
+        //Step 12, repeat start to begin change from write to read
+        $I2C_12
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 13
+        JUMP $RepeatStartI2C
+
+        //Step 13, write address light sens, set to Read
+        //I2C will swap to Master/Receiver mode
+        $I2C_13
+        APBREAD 1 0x04
+        CMP DAT8 0x10
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b1
+        IOWRT DAT8 14
+        JUMP $ContinueI2C
+
+        //Step 14, prep to read CH1_1 register data
+        $I2C_14
+        APBREAD 1 0x04
+        CMP DAT8 0x40
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 15
+        JUMP $ContinuteI2CRead
+
+        //Step 15, actually finally read CH11 register data
+        $I2C_15
+        APBREAD 1 0x04
+        CMP DAT8 0x58
+        JUMP IFNOT ZERO $AbandonShip
+        APBREAD 1 0x08
+        APBWRT ACC 0 0x02
+        IOWRT DAT8 16
+        JUMP $StopStartI2C
+    
+    //END Light Sens Read CH1_1
+
+    //BEGIN Light Sens Read CH0_0
+        //Step 16, write address light sens for CH0_0 read process
+        $I2C_16
+        APBREAD 1 0x04
+        CMP DAT8 0x08
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b0
+        IOWRT DAT8 17
+        JUMP $ContinueI2C
+
+        //Step 17, write CH0_0 register address
+        $I2C_17
+        APBREAD 1 0x04
+        CMP DAT8 0x18
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0x8A
+        IOWRT DAT8 18
+        JUMP $ContinueI2C
+
+        //Step 18, repeat start to begin change from write to read
+        $I2C_18
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 19
+        JUMP $RepeatStartI2C
+
+        //Step 19, write address light sens, set to Read
+        //I2C will swap to Master/Receiver mode
+        $I2C_19
+        APBREAD 1 0x04
+        CMP DAT8 0x10
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b1
+        IOWRT DAT8 20
+        JUMP $ContinueI2C
+
+        //Step 20, prep to read CH0_0 register data
+        $I2C_20
+        APBREAD 1 0x04
+        CMP DAT8 0x40
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 21
+        JUMP $ContinuteI2CRead
+
+        //Step 21, actually finally read CH0_0 register data
+        $I2C_21
+        APBREAD 1 0x04
+        CMP DAT8 0x58
+        JUMP IFNOT ZERO $AbandonShip
+        APBREAD 1 0x08
+        APBWRT ACC 0 0x01
+        IOWRT DAT8 22
+        JUMP $StopStartI2C
+
+    //END Light Sens Read CH0_0
+
+    //BEGIN Light Sens Read CH0_1
+        //Step 22, write address light sens for CH0_1 read process
+        $I2C_22
+        APBREAD 1 0x04
+        CMP DAT8 0x08
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b0
+        IOWRT DAT8 23
+        JUMP $ContinueI2C
+
+        //Step 23, write CH0_1 register address
+        $I2C_23
+        APBREAD 1 0x04
+        CMP DAT8 0x18
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 0x8B
+        IOWRT DAT8 24
+        JUMP $ContinueI2C
+
+        //Step 24, repeat start to begin change from write to read
+        $I2C_24
+        APBREAD 1 0x04
+        CMP DAT8 0x28
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 25
+        JUMP $RepeatStartI2C
+
+        //Step 25, write address light sens, set to Read
+        //I2C will swap to Master/Receiver mode
+        $I2C_25
+        APBREAD 1 0x04
+        CMP DAT8 0x10
+        JUMP IFNOT ZERO $AbandonShip
+        APBWRT DAT8 1 0x08 (0x29 << 1) | 0b1
+        IOWRT DAT8 26
+        JUMP $ContinueI2C
+
+        //Step 26, prep to read CH0_1 register data
+        $I2C_26
+        APBREAD 1 0x04
+        CMP DAT8 0x40
+        JUMP IFNOT ZERO $AbandonShip
+        IOWRT DAT8 27
+        JUMP $ContinuteI2CRead
+
+        //Step 27, actually finally read CH0_1 register data
+        $I2C_27
+        APBREAD 1 0x04
+        CMP DAT8 0x58
+        JUMP IFNOT ZERO $AbandonShip
+        APBREAD 1 0x08
+        APBWRT ACC 0 0x01
+        //GO BACK TO STEP 4
+        IOWRT DAT8 4
+        JUMP $StopI2C
+    
+    //END Light Sens Read CH0_1
+
+    //END LIGHT SENS READ STATES
+
+
+    //BEGIN I2C shared Functions?
+
+        $ContinuteI2CRead
+        APBREAD 1 0x00
+        AND DAT8 0b11000011
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $ContinueI2C
+        APBREAD 1 0x00
+        AND DAT8 0b11000111
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $RepeatStartI2C
+        APBREAD 1 0x00
+        AND DAT8 0b11110111
+        OR DAT8 0b00100000
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $StopStartI2C
+        APBREAD 1 0x00
+        AND DAT8 0b11110111
+        OR DAT8 0b00110000
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $StopI2C
+        APBREAD 1 0x00
+        AND DAT8 0b11110111
+        OR DAT8 0b00010000
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $ClearIntI2C
+        APBREAD 1 0x00
+        AND DAT8 0b11110111
+        APBWRT ACC 1 0x00
+        RETISR
+
+        $AbandonShip
+        //Abandon i2c message, reset, whatever
+        IOWRT DAT8 0xFF
+        JUMP $StopI2C
+
+    //END I2C shared function things
+
+
+    //Handle APB_Slave interrupt, custom logic, its magic
+    $INTAPBSLAVE
+    
+    $EXITISR
+    RETISR
+    
+$MAIN
+    //Set Timer Prescale to PCLK/128 (0b0110)
+    APBWRT DAT8 2 0x0C 0b0110
+    //Load Timer with round(39062.5)
+    APBWRT DAT 2 0x00 39063
+    //Start Timer
+    APBWRT DAT8 2 0x08 0b011
+
+
+    //Initialize I2C
+    //Set I2C CLK to  PCLK/60 (0b110)
+        //Set in GUI
+    //Send I2C Start
+    APBREAD 1 0x00
+    OR DAT8 0b01100000
+    APBWRT ACC 1 0x00
+    
+    //Write I2C init step to feedbackIO
+    IOWRT DAT8 0
+    
+
+    APBWRT DAT8 0 0xFF 0x00
+    APBWRT DAT8 0 0xF0 0x00
+
+HALT
